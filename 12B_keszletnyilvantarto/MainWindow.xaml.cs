0d8e0df6e77372597418b62f3dd22188d2bcfe8f
Gergely Viktor <viktor.gergely@gmail.com>
2025-04-28 10:56:13 +0200

    4-es feladat kész

--- 12B_keszletnyilvantarto/MainWindow.xaml.cs
+++ 12B_keszletnyilvantarto/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,18 @@ namespace _12B_keszletnyilvantarto {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        MySqlConnection kapcs = new MySqlConnection("server = server.fh2.hu;database = v2labgwj_12b; uid = v2labgwj_12b; password = '4W56FNhfKJfeZVhGwasG';");
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+
         }
     }
 }
